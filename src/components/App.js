bcf9f8c24615a56f8eac47976b14780bafa97de6
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-10-30 15:49:42 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dog, setDog] = useState(null); // Initialize dog as null
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setDog(data.message);
+      });
+  }, []);
+
+  if (dog === null) return <p>Loading...</p>; // Check if dog is null and display loading message
+
+  return (
+    <img src={dog} alt="A Random Dog" />
+  );
+}
+
+export default App;
